19870bcd23f2bc14d3b3210073ca9fae76835fde
treeinsect <swright9332@gmail.com>
2024-10-27 02:52:35 +0000

    WORKING sensor file

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,43 +1,47 @@
-//the code is SOMEWHAT created by: SHEEKAR BANERJEE (at December 2017) 
-//Dept. of CSE, IUBAT 
-//AI-ML-IOT Solution Engineer and Researcher 
-//it is also created by JACK BABEY, WILL WRIGHT and SAMMY SAHNINE as part of HackNotts 24!!!!!
-const int AOUTpin= A0; 
-const int DOUTpin= D3; 
+//sensor
+const int AOUTpin = A0;//the analogue pin of the alcohol sensor goes into analog  pin A0 of the arduino
 
-const int out0 = D0;
-const int out1 = D2;
+//leds
+const int yellow = D5;
+const int green = D6;
+const int red = D7;
+const int white = D1;
 
-int limit; 
-int value; 
+const int wait = 10000;
+int value;
 
-void setup() { 
-	   Serial.begin(9600);//sets the baud rate 
-	   pinMode(DOUTpin, INPUT); 
-     pinMode(out0, OUTPUT);
-     pinMode(out1, OUTPUT);
-} 
 
-void loop() 
-{ 
-  value = analogRead(AOUTpin); 
+void  setup() {
+  //sensor
+  Serial.begin(9600);//sets the baud rate
+  pinMode(AOUTpin, INPUT);
+  
+  //leds
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(white, OUTPUT);
+}
 
-  Serial.print("Alcohol value: "); 
-  Serial.println(value); 
+void loop()
+{
 
-  if (value < 400) {
-    Serial.println("1000 DRACULAS? MORE LIKE 1000 BEERS. GET DRINKING!");
-    digitalWrite(out0, LOW);
-    digitalWrite(out1, LOW);
-  } else if (value > 600) {
-    Serial.println("*CAR CRASH SFX* DON'T GET NEAR A VEHICLE - YOU'VE HAD TOO MUCH!");
-    digitalWrite(out0, HIGH);
-    digitalWrite(out1, HIGH);
+  value = analogRead(AOUTpin);//reads  the analaog value from the alcohol sensor's AOUT pin
+  Serial.print("Alcohol value:  ");
+  Serial.println(value);
+
+  if (value < 400){
+    digitalWrite(yellow, HIGH);
+    digitalWrite(green, LOW);
+    digitalWrite(red, LOW);
+  } else if (value > 550){
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(red, HIGH);
   } else {
-    Serial.println("BALLMER PEAK ACHIEVED !! OPEN VSCODE NOW");
-    digitalWrite(out0, HIGH);
-    digitalWrite(out1, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, HIGH);
+    digitalWrite(red, LOW);
   }
 
-  delay(1000); 
 }
